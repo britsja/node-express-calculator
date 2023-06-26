e740eba916049dd113a8ef0f48dae5323c145ca1
Jan <pcerservices@gmail.com>
2023-06-26 13:27:10 +0200

    Added body-parser, index.html with form and function to add numbers

--- calculator.js
+++ calculator.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParses = require('body-parser')
 const app = express();
+app.use(bodyParses.urlencoded({extended: true}));
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 });
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+  var result = parseInt(req.body.num1) + parseInt(req.body.num2);
+  res.send(`Your calculated total is: ${result}`);
 })
